6745de28295f004f93b84ebac36947a9bc73f6df
LucasChang3007 <lucasfelipechangalegre@hotmail.com>
2025-05-13 15:14:56 -0300

    commit teste Lucas

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const testeLucas = 'teste string'
     return 'Hello World!';
   }
 }
